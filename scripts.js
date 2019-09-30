bbeddb42e6cad96b573721795667c50ea2d4eb1a
leonardo Kaplan <kaplanleonardo@gmail.com>
2019-09-30 11:48:44 -0500

    setting up function that created all the divs for each cell

--- scripts.js
+++ scripts.js
@@ -1,11 +1,11 @@
 // alert("yolo! binches");
 let gridDiv = document.createElement("div");
-let actualGrid = document.createElement("div");
+//let actualGrid = document.createElement("div");
 const bodyNode = document.querySelector("body");
 
 bodyNode.appendChild(gridDiv);
-gridDiv.appendChild(actualGrid);
+//gridDiv.appendChild(actualGrid);
 
 gridDiv.setAttribute("id","gridDiv");
-actualGrid.setAttribute("id","grid");
-gridDiv.style.cssText = " display: grid; border: 1px red solid; width: 34em; height: 34em;  grid-template-columns: repeat(16, 1fr); grid-template-rows: repeat(16,1fr);"
+//actualGrid.setAttribute("id","grid");
+gridDiv.style.cssText = " display: grid; border: 1px red solid; width: 34em; height: 34em; grid-template-columns: repeat(16, 1fr); grid-template-rows: repeat(16,1fr);"
