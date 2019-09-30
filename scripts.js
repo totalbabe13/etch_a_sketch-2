423b257708c3ee04c4af52ed3482428d45ecc901
leonardo Kaplan <kaplanleonardo@gmail.com>
2019-09-30 14:10:23 -0500

    clear button refreshes grid to be blank

--- scripts.js
+++ scripts.js
@@ -1,10 +1,14 @@
 // alert("yolo! binches");
-let gridDiv = document.createElement("div");
+let gridDiv    = document.createElement("div");
 const bodyNode = document.querySelector("body");
+let clearGridBtn  = document.createElement("button");
+bodyNode.appendChild(clearGridBtn);
 bodyNode.appendChild(gridDiv);
 
 gridDiv.setAttribute("id","gridDiv");
-gridDiv.style.cssText = " display: grid; border: 1px red solid; width: 34em; height: 34em; grid-template-columns: repeat(16, 1fr); grid-template-rows: repeat(16,1fr);"
+gridDiv.style.cssText = "margin-top: 3vh; margin-left:auto; margin-right: auto; display: grid; border: 1px red solid; width: 34em; height: 34em; grid-template-columns: repeat(16, 1fr); grid-template-rows: repeat(16,1fr);"
+clearGridBtn.style.cssText = "margin-left: 19vh; background-color: #2196f3;"
+clearGridBtn.textContent = "Clear the Grid"
 
 function createGridCells(){
   for (i = 0; i < 256; i++){
@@ -21,13 +25,12 @@ function createGridCells(){
 };
 
 createGridCells();
-// add hover mouseover to every cell
 
-// select every cell
-// let allCells = document.getElementsByClassName("cell")
-// allCells.onmouseover = fillCell;
-//
-// function fillCell(){
-//   console.log("test");
-  //allCells.style.cssText =  "background: blue;" //"background: #00ff29;"
-//}
+clearGridBtn.onclick = clearGrid;
+function clearGrid(){
+   let cells = document.querySelectorAll(".cell")
+   //console.log(cells)
+   for (i = 0; i < cells.length; ++i) {
+   cells[i].style.cssText = "background: white;";
+  }
+};
